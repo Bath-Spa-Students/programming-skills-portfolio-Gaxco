95948558ad590e6ebdcb7959c5acefce0622074a
Gaxco <athulgeorge1905@gmail.com>
2023-11-22 12:35:53 +0400

    Chapter 3 Exercises

---
+++ Chapter 3- Data Structures/exercise1.py
@@ -0,0 +1,5 @@
+# Print each person’s name by accessing each element in the list, one at a time.
+names=["athul","imran","sadiq"]
+print(names[0])
+print(names[1])
+print(names[2])
---
+++ Chapter 3- Data Structures/exercise4.py
@@ -0,0 +1,8 @@
+#invite peopele to dinner
+guests=["athul","imran","jeevan"]
+name= guests[0].title()
+print(name + ", please come to dinner.")
+name= guests[1].title()
+print(name + ", please come to dinner.")
+name= guests[2].title()
+print(name + ", please come to dinner.")
